bfcacb258cf93d0c6f391bcd1788f2e7a0fe3a64
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2025-03-15 21:27:04 +0530

    Print all Spring beans while application run
    Print all Spring beans while application run

--- src/main/java/com/sid/app/TrackerManagerService.java
+++ src/main/java/com/sid/app/TrackerManagerService.java
@@ -2,6 +2,9 @@ package com.sid.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class TrackerManagerService {
@@ -10,4 +13,12 @@ public class TrackerManagerService {
         SpringApplication.run(TrackerManagerService.class, args);
     }
 
+    // Print all the Bean names
+    /*
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(TrackerManagerService.class, args);
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        Arrays.stream(beanDefinitionNames).sorted().forEach(System.out::println);
+    }
+    */
 }
