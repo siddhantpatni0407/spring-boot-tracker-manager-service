c6b43309b5a739ec04c8ca71fa55e0157776ba19
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2025-03-16 13:51:22 +0530

    Updated imports
    Updated imports

--- src/main/java/com/sid/app/entity/FuelExpense.java
+++ src/main/java/com/sid/app/entity/FuelExpense.java
@@ -1,8 +1,19 @@
 package com.sid.app.entity;
 
 import com.sid.app.audit.Auditable;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
